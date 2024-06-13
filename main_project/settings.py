e9db4e2355016f3f71018ca1b16d04a5479ca342
SRIDHAR-08 <sridhar200288@gmail.com>
2024-06-13 21:03:21 +0530

    data

--- main_project/settings.py
+++ main_project/settings.py
@@ -67,16 +67,23 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'portfoloi2',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': '12345',
+#         'PORT':'3306'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfoloi2',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '12345',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
