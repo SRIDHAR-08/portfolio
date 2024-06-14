06d00983b0419187bfea2be836ceb9678a8ffa02
SRIDHAR-08 <sridhar200288@gmail.com>
2024-06-14 09:13:33 +0530

    sql

--- main_project/settings.py
+++ main_project/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+# ofline sql
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -77,12 +77,28 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 #         'PORT':'3306'
 #     }
 # }
+
+# online sql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bmqakkwcaadqa4somr4n',
+        'USER': 'u0r3r9vdncw1vrp5',
+        'PASSWORD': 'NmtRj0ifYDH9q4WJxnIW',
+        'HOST': 'bmqakkwcaadqa4somr4n-mysql.services.clever-cloud.com',
+        'PORT': '3306',
     }
 }
+
+
+# sqlite
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
