ed100f147d99e9ab1904daa30f2c1b472ea88524
SRIDHAR-08 <sridhar200288@gmail.com>
2024-06-14 09:41:42 +0530

    fdg

--- main_project/settings.py
+++ main_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-87rme8s!02lwnq#ew-2kb8hipzms*3wb98b*_fday(glsk1=r(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
